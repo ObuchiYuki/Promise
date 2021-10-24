a578c6a6b109713fc17e4d4ddcbf26eba2c5ef56
Obuchi Yuki <yukibochi1@gmail.com>
2021-10-24 10:46:41 +0900

    add data integration

---
+++ Sources/Promise/Integration/Promise+Data.swift
@@ -0,0 +1,16 @@
+//
+//  Promise+Data.swift
+//  
+//
+//  Created by yuki on 2021/10/24.
+//
+
+import Foundation
+
+extension Data {
+    @inlinable public static func async(contentsOf url: URL) -> Promise<Data, Error> {
+        Promise.asyncError(on: .global()) { resolve, _ in
+            try resolve(self.init(contentsOf: url))
+        }
+    }
+}
