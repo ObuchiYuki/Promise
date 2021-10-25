8de38cde7c76d2ca99815fc94263cee50e7e18cb
Obuchi Yuki <yukibochi1@gmail.com>
2021-10-25 17:20:48 +0900

    modelcoreadd wait

--- Sources/Promise/Promise+Wait.swift
+++ Sources/Promise/Promise+Wait.swift
@@ -25,6 +25,12 @@ extension Promise {
         }
     }
     
+    public func wait<T, F>(_ promise: @escaping () -> Promise<T, F>) -> Promise<Output, Error> {
+        Promise<Output, Error> { resolve, reject in
+            self.sink({ output in promise().sink({_ in resolve(output) }, reject) }, reject)
+        }
+    }
+    
     public static func wait(on queue: DispatchQueue = .main, for interval: TimeInterval) -> Promise<Output, Failure> where Output == Void, Failure == Never {
         Promise(output: ()).wait(on: queue, for: interval)
     }
