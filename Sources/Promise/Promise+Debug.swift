5df8f4771e76567181bfad0908ba5b9496fe069f
Obuchi Yuki <yukibochi1@gmail.com>
2021-10-28 12:10:58 +0900

    remove inline

--- Sources/Promise/Promise+Debug.swift
+++ Sources/Promise/Promise+Debug.swift
@@ -9,12 +9,56 @@
 import Foundation
 
 extension Promise {
-    @inlinable public func breakOnFailure() -> Promise<Output, Failure> {
-        self.catch{ error in
-            Swift.print(error)
-            raise(SIGTRAP)
+    public func breakpointOnError() -> Promise<Output, Failure> {
+        self.breakpoint(nil, {_ in true })
+    }
+    
+    public func breakpoint(_ receiveOutput: ((Output) -> Bool)? = nil, _ receiveFailure: ((Failure) -> Bool)? = nil) -> Promise<Output, Failure> {
+        #if DEBUG
+        return Promise<Output, Failure>{ resolve, reject in
+            self.subscribe({ output in
+                if receiveOutput?(output) == true { raise(SIGTRAP) }
+                resolve(output)
+            }, { failure in
+                if receiveFailure?(failure) == true { raise(SIGTRAP) }
+                reject(failure)
+            })
         }
+        #else
         return self
+        #endif
+    }
+    
+    public func assertNoFailure(_ prefix: String = "", file: StaticString = #file, line: UInt = #line) -> Promise<Output, Never> {
+        Promise<Output, Never>{ resolve, _ in
+            self.subscribe(resolve, { error in
+                let prefix = prefix.isEmpty ? "" : prefix + ": "
+                fatalError("\(prefix)\(error)", file: file, line: line)
+            })
+        }
+    }
+    
+    final private class PrintTarget: TextOutputStream {
+        var stream: TextOutputStream
+        func write(_ string: String) { stream.write(string) }
+        init(stream: TextOutputStream) { self.stream = stream }
+    }
+    
+    public func print(_ prefix: String = "", to stream: TextOutputStream? = nil) -> Promise<Output, Failure> {
+        let prefix = prefix.isEmpty ? "" : "\(prefix): "
+        let stream = stream.map(PrintTarget.init)
+        
+        func log(_ text: String) {
+            if var stream = stream { Swift.print(text, to: &stream) } else { Swift.print(text) }
+        }
+        
+        return Promise<Output, Failure>{ resolve, reject in
+            self.subscribe({ output in
+                log("\(prefix)receive output: (\(output))"); resolve(output)
+            }, { failure in
+                log("\(prefix)receive failure: (\(failure))"); reject(failure)
+            })
+        }
     }
     
     @inlinable public func measure(_ prefix: String?) -> Promise<Output, Failure> {
@@ -27,20 +71,6 @@ extension Promise {
             printPrefix()
             Swift.print("measure end:", "\(interval) s")
         }
-        return self
-    }
-    
-    @inlinable public func print(_ prefix: String?) -> Promise<Output, Failure> {
-        func printPrefix() {
-            if let prefix = prefix { Swift.print("\(prefix): ", terminator: "") }
-        }
-        self.sink({ output in
-            printPrefix()
-            Swift.print("receive output:", "(\(output))")
-        }, { failure in
-            printPrefix()
-            Swift.print("receive failure:", "(\(failure))")
-        })
         return self
     }
 }
--- Sources/Promise/Promise.swift
+++ Sources/Promise/Promise.swift
@@ -12,30 +12,17 @@ public final class Promise<Output, Failure: Error> {
         case rejected(Failure)
     }
     
-    @usableFromInline struct Subscriber {
+    struct Subscriber {
         public let resolve: (Output) -> ()
         public let reject: (Failure) -> ()
-        
-        @inlinable init(resolve: @escaping (Output) -> (), reject: @escaping (Failure) -> ()) {
-            self.resolve = resolve
-            self.reject = reject
-        }
     }
 
-    @usableFromInline var state = State.pending
-    @usableFromInline var subscribers = [Subscriber]()
+    public private(set) var state = State.pending
+    var subscribers = [Subscriber]()
     
-    @inlinable init() {}
-
-    @inlinable func subscribe(_ resolve: @escaping (Output) -> (), _ reject: @escaping (Failure) -> ()) {
-        switch self.state {
-        case .pending: self.subscribers.append(Subscriber(resolve: resolve, reject: reject))
-        case .fulfilled(let output): resolve(output)
-        case .rejected(let failure): reject(failure)
-        }
-    }
+    public init() {}
     
-    @inlinable public func fullfill(_ output: Output) {
+    public func fullfill(_ output: Output) {
         guard case .pending = self.state else { return }
         
         self.state = .fulfilled(output)
@@ -43,53 +30,55 @@ public final class Promise<Output, Failure: Error> {
         self.subscribers.removeAll()
     }
     
-    @inlinable public func reject(_ failure: Failure) {
+    public func reject(_ failure: Failure) {
         guard case .pending = self.state else { return }
         
         self.state = .rejected(failure)
         for subscriber in self.subscribers { subscriber.reject(failure) }
         self.subscribers.removeAll()
     }
+
+    func subscribe(_ resolve: @escaping (Output) -> (), _ reject: @escaping (Failure) -> ()) {
+        switch self.state {
+        case .pending: self.subscribers.append(Subscriber(resolve: resolve, reject: reject))
+        case .fulfilled(let output): resolve(output)
+        case .rejected(let failure): reject(failure)
+        }
+    }
 }
 
 extension Promise {
-    @inlinable public convenience init(_ handler: (@escaping (Output) -> (), @escaping (Failure) -> ()) -> ()) {
+    public convenience init(_ handler: (@escaping (Output) -> (), @escaping (Failure) -> ()) -> ()) {
         self.init()
         handler(self.fullfill, self.reject)
     }
-    @inlinable public convenience init(_ handler: (@escaping (Output) -> (), @escaping (Failure) -> ()) throws -> ()) where Failure == Error {
+    public convenience init(_ handler: (@escaping (Output) -> (), @escaping (Failure) -> ()) throws -> ()) where Failure == Error {
         self.init()
         do { try handler(self.fullfill, self.reject) } catch { self.reject(error) }
     }
     
-    @inlinable public convenience init(output: Output) {
+    public convenience init(output: Output) {
         self.init()
         self.fullfill(output)
     }
     
-    @inlinable public convenience init(failure: Failure) {
+    public convenience init(failure: Failure) {
         self.init()
         self.reject(failure)
     }
     
-    @inlinable public convenience init(output: @autoclosure () throws -> Output) where Failure == Error {
+    public convenience init(output: @autoclosure () throws -> Output) where Failure == Error {
         self.init()
         do { self.fullfill(try output()) } catch { self.reject(error) }
     }
     
-    @inlinable public static func pending() -> Promise<Output, Failure> {
+    public static func pending() -> Promise<Output, Failure> {
         Promise()
     }
 }
 
-extension Promise {
-    @inlinable public func getState() -> State {
-        self.state
-    }
-}
-
 extension Promise: CustomStringConvertible {
-    @inlinable public var description: String {
+    public var description: String {
         "Promise<\(Output.self), \(Failure.self)>(\(self.state))"
     }
 }
