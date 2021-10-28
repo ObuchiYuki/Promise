426de52353405eff6d1f82a7fc44dab9ae7fc50f
Obuchi Yuki <yukibochi1@gmail.com>
2021-10-28 12:52:55 +0900

    add settle

---
+++ Sources/Promise/Promise+Settle.swift
@@ -0,0 +1,13 @@
+//
+//  Promise+Settle.swift
+//  
+//
+//  Created by yuki on 2021/10/28.
+//
+
+extension Promise.State {
+    public var isSettled: Bool {
+        if case .pending = self { return true }
+        return false
+    }
+}
