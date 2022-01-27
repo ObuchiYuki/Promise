6edeedb60a9c202b57c1dbe2aa2e820cad7d0dcc
Obuchi Yuki <yukibochi1@gmail.com>
2022-01-27 11:19:00 +0900

    fix sink

--- Sources/Promise/Promise+Cancel.swift
+++ Sources/Promise/Promise+Cancel.swift
@@ -16,22 +16,22 @@ public struct PromiseCancelError: LocalizedError {
 extension Promise {
     public func cancelled<T>(by canceller: Promise<T, Never>) -> Promise<Output, Error> {
         Promise<Output, Error>{ resolve, reject in
-            canceller.sink{ reject(PromiseCancelError(object: $0)) }
-            self.sink(resolve, reject)
+            canceller.subscribe({ reject(PromiseCancelError(object: $0)) }, {_ in})
+            self.subscribe(resolve, reject)
         }
     }
 
     public func cancelled<T>(by canceller: Promise<T, Never>) -> Promise<Output, PromiseCancelError> where Failure == Never {
         Promise<Output, PromiseCancelError>{ resolve, reject in
-            canceller.sink{ reject(PromiseCancelError(object: $0)) }
-            self.sink(resolve)
+            canceller.subscribe({ reject(PromiseCancelError(object: $0))}, {_ in})
+            self.subscribe(resolve, {_ in})
         }
     }
     
     @discardableResult
     public func catchCancel(by handler: @escaping (Any) -> () = {_ in}) -> Promise<Void, Failure> {
         Promise<Void, Failure> { resolve, reject in
-            self.sink({_ in resolve(()) }, { error in
+            self.subscribe({_ in resolve(()) }, { error in
                 if let error = error as? PromiseCancelError {
                     handler(error.object)
                     resolve(())
@@ -45,7 +45,7 @@ extension Promise {
     @discardableResult
     public func catchCancel(by handler: @escaping (Any) -> () = {_ in}) -> Promise<Void, Never> where Failure == PromiseCancelError {
         Promise<Void, Never> { resolve, reject in
-            self.sink({_ in resolve(()) }, { error in
+            self.subscribe({_ in resolve(()) }, { error in
                 handler(error.object)
                 resolve(())
             })
