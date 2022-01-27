25d31507a9e6c03bc5f5ff21d038683b6bd37415
Obuchi Yuki <yukibochi1@gmail.com>
2022-01-27 09:08:44 +0900

    remove timeout

---
+++ Sources/Promise/Integration/Promise+Timeout.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by yuki on 2022/01/27.
+//
+
+import Foundation
+
+public struct PromiseTimeoutError: LocalizedError {
+    public let timeoutInterval: TimeInterval
+    
+    public var errorDescription: String { "PromiseTimeoutError: Timeout Interval \(timeoutInterval) s exceeded." }
+}
+
+extension Promise where Failure == Never {
+    public func timeout(with timeoutInterval: TimeInterval) -> Promise<Output, PromiseTimeoutError> {
+        Promise<Output, PromiseTimeoutError>{ resolve, reject in
+            self.sink(resolve)
+            Timer.scheduledTimer(withTimeInterval: timeoutInterval, repeats: false, block: { timer in
+                reject(PromiseTimeoutError(timeoutInterval: timeoutInterval))
+            })
+        }
+    }
+}
+
+extension Promise {
+    public func timeout(with timeoutInterval: TimeInterval) -> Promise<Output, Error> {
+        Promise<Output, Error>{ resolve, reject in
+            self.sink(resolve, reject)
+            Timer.scheduledTimer(withTimeInterval: timeoutInterval, repeats: false, block: { timer in
+                reject(PromiseTimeoutError(timeoutInterval: timeoutInterval))
+            })
+        }
+    }
+}
