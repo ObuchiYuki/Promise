43641ae834cf88e367dc1d36c3a0fa066ff3749f
Obuchi Yuki <yukibochi1@gmail.com>
2022-02-18 23:21:16 +0900

    update cancel

--- Sources/Promise/Promise+Cancel.swift
+++ Sources/Promise/Promise+Cancel.swift
@@ -8,32 +8,30 @@
 import Foundation
 
 public struct PromiseCancelError: LocalizedError {
-    public let object: Any
-    
-    public var errorDescription: String? { "Promise has been cancelled with '\(object)'." }
+    public var errorDescription: String? { "Promise has been cancelled." }
 }
 
 extension Promise {
     public func cancelled<T>(by canceller: Promise<T, Never>) -> Promise<Output, Error> {
         Promise<Output, Error> { resolve, reject in
-            canceller.subscribe({ reject(PromiseCancelError(object: $0)) }, {_ in})
+            canceller.subscribe({_ in reject(PromiseCancelError()) }, {_ in})
             self.subscribe(resolve, reject)
         }
     }
 
     public func cancelled<T>(by canceller: Promise<T, Never>) -> Promise<Output, PromiseCancelError> where Failure == Never {
         Promise<Output, PromiseCancelError> { resolve, reject in
-            canceller.subscribe({ reject(PromiseCancelError(object: $0))}, {_ in})
+            canceller.subscribe({_ in reject(PromiseCancelError())}, {_ in})
             self.subscribe(resolve, {_ in})
         }
     }
     
     @discardableResult
-    public func catchCancel(by handler: @escaping (Any) -> () = {_ in}) -> Promise<Void, Failure> {
+    public func catchCancel(by handler: @escaping () -> ()) -> Promise<Void, Failure> {
         Promise<Void, Failure> { resolve, reject in
             self.subscribe({_ in resolve(()) }, { error in
-                if let error = error as? PromiseCancelError {
-                    handler(error.object)
+                if error is PromiseCancelError {
+                    handler()
                     resolve(())
                 } else {
                     reject(error)
@@ -43,10 +41,10 @@ extension Promise {
     }
     
     @discardableResult
-    public func catchCancel(by handler: @escaping (Any) -> () = {_ in}) -> Promise<Void, Never> where Failure == PromiseCancelError {
+    public func catchCancel(by handler: @escaping () -> ()) -> Promise<Void, Never> where Failure == PromiseCancelError {
         Promise<Void, Never> { resolve, reject in
             self.subscribe({_ in resolve(()) }, { error in
-                handler(error.object)
+                handler()
                 resolve(())
             })
         }
