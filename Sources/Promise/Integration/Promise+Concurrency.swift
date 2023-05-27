301b81172a56116c5230865f92592d2223c56dbf
ObuchiYuki <yukibochi1@gmail.com>
2023-05-27 22:14:11 +0900

    add concurrency support

--- Sources/Promise/Integration/Promise+Concurrency.swift
+++ Sources/Promise/Integration/Promise+Concurrency.swift
@@ -5,18 +5,37 @@
 //  Created by yuki on 2023/03/17.
 //
 
+@available(iOS 13.0.0, *) @available(macOS 10.15.0, *)
 extension Promise {
-    @available(iOS 13.0.0, *) @available(macOS 10.15.0, *)
     public func value() async throws -> Output {
         try await withCheckedThrowingContinuation{ continuation in
             self.sink(continuation.resume(returning:), continuation.resume(throwing:))
         }
     }
 
-    @available(iOS 13.0.0, *) @available(macOS 10.15.0, *)
     public func value() async -> Output where Failure == Never {
         await withCheckedContinuation{ continuation in
             self.sink(continuation.resume(returning:), continuation.resume(throwing:))
+        }
+    }
+    
+    public convenience init(_ task: @escaping () async -> Output) where Failure == Never {
+        self.init()
+        
+        Task{
+            self.fulfill(await task())
+        }
+    }
+    
+    public convenience init(_ task: @escaping () async throws -> Output) where Failure == Error {
+        self.init()
+        
+        Task{
+            do {
+                self.fulfill(try await task())
+            } catch {
+                self.reject(error)
+            }
         }
     }
 }
