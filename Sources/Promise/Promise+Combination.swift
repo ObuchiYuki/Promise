d9caaedeaf1e0c17f5da51f6de7cedc580f5b7d7
ObuchiYuki <yukibochi1@gmail.com>
2023-05-29 12:43:08 +0900

    add merge

--- Sources/Promise/Promise+Combination.swift
+++ Sources/Promise/Promise+Combination.swift
@@ -6,6 +6,32 @@
 //
 
 extension Promise {
+    public func merge(_ a: Promise<Output, Failure>) -> Promise<Output, Failure> {
+        let promise = Promise<Output, Failure>()
+        self.subscribe(promise.fulfill, promise.reject)
+        a.subscribe(promise.fulfill, promise.reject)
+        return promise
+    }
+    
+    public func merge(_ a: Promise<Output, Failure>, _ b: Promise<Output, Failure>) -> Promise<Output, Failure> {
+        let promise = Promise<Output, Failure>()
+        self.subscribe(promise.fulfill, promise.reject)
+        a.subscribe(promise.fulfill, promise.reject)
+        b.subscribe(promise.fulfill, promise.reject)
+        return promise
+    }
+    
+    public func merge(_ a: Promise<Output, Failure>, _ b: Promise<Output, Failure>, _ c: Promise<Output, Failure>) -> Promise<Output, Failure> {
+        let promise = Promise<Output, Failure>()
+        self.subscribe(promise.fulfill, promise.reject)
+        a.subscribe(promise.fulfill, promise.reject)
+        b.subscribe(promise.fulfill, promise.reject)
+        c.subscribe(promise.fulfill, promise.reject)
+        return promise
+    }
+}
+
+extension Promise {
     public func combine<A>(_ a: Promise<A, Failure>) -> Promise<(Output, A), Failure> {
         Promise.combine(self, a)
     }
@@ -15,9 +41,7 @@ extension Promise {
     public func combine<A, B, C>(_ a: Promise<A, Failure>, _ b: Promise<B, Failure>, _ c: Promise<C, Failure>) -> Promise<(Output, A, B, C), Failure> {
         Promise.combine(self, a, b, c)
     }
-}
-
-extension Promise {
+    
     public static func combine<A, B>(_ a: Promise<A, Failure>, _ b: Promise<B, Failure>) -> Promise<(A, B), Failure> {
         Promise<(A, B), Failure>{ resolve, reject in
             var outputA: A?
@@ -89,20 +113,35 @@ extension Promise {
 }
 
 
-extension Array where Element: _PromiseCombineAllInterface {
+extension Array where Element: _PromiseCombineInterface {
+    public func mergeAll() -> Promise<Element.Output, Element.Failure> {
+        Element._mergeAll(self)
+    }
+    
     public func combineAll() -> Promise<[Element.Output], Element.Failure> {
         Element._combineAll(self)
     }
 }
 
-public protocol _PromiseCombineAllInterface {
+public protocol _PromiseCombineInterface {
     associatedtype Output
     associatedtype Failure: Error
     
     static func _combineAll(_ promises: [Self]) -> Promise<[Output], Failure>
+    static func _mergeAll(_ promises: [Self]) -> Promise<Output, Failure>
 }
 
-extension Promise: _PromiseCombineAllInterface {
+extension Promise: _PromiseCombineInterface {
+    public static func _mergeAll(_ promises: [Promise<Output, Failure>]) -> Promise<Output, Failure> {
+        let promise = Promise<Output, Failure>()
+        
+        for sub in promises {
+            sub.subscribe(promise.fulfill, promise.reject)
+        }
+        
+        return promise
+    }
+    
     public static func _combineAll(_ promises: [Promise<Output, Failure>]) -> Promise<[Output], Failure> {
         if promises.isEmpty { return Promise<[Output], Failure>.resolve([]) }
         
