82be29f4db714318930e7ca452a7817d4721606a
ObuchiYuki <yukibochi1@gmail.com>
2023-08-17 23:47:53 +0900

    move test

--- Tests/PromiseTests/Promise+Test.swift
+++ Tests/PromiseTests/Promise+Test.swift
@@ -5,8 +5,8 @@
 //  Created by yuki on 2023/08/09.
 //
 
-#if canImport(XCTest)
 import XCTest
+import Promise
 
 extension Promise {
     public func waitUntilExit(_ testCase: XCTestCase, timeout: TimeInterval = 1) {
@@ -17,4 +17,3 @@ extension Promise {
         testCase.wait(for: [expectation], timeout: timeout)
     }
 }
-#endif
