82db868fc3219f5b239f50b4251645a90c53a2c8
ObuchiYuki <yukibochi1@gmail.com>
2023-11-15 14:52:56 +0900

    [fix] fix timeout

--- Sources/Promise/Ex+Foundation/Promise+Timeout.swift
+++ Sources/Promise/Ex+Foundation/Promise+Timeout.swift
@@ -8,31 +8,21 @@
 #if canImport(Foundation)
 import Foundation
 
-public struct PromiseTimeoutError: Error, CustomStringConvertible {
-    public let timeoutInterval: TimeInterval
-    
-    public var description: String { "PromiseTimeoutError: Timeout interval \(timeoutInterval) s exceeded." }
-    
-    @inlinable init(timeoutInterval: TimeInterval) {
-        self.timeoutInterval = timeoutInterval
-    }
-}
-
 extension Promise {
-    @inlinable public func timeout(_ interval: TimeInterval, on queue: DispatchQueue = .global()) -> Promise<Output, Error> {
+    @inlinable public func timeout<T: Error>(_ interval: TimeInterval, error: @autoclosure @escaping () -> T, on queue: DispatchQueue = .global()) -> Promise<Output, Error> {
         let promise = Promise<Output, Error>()
         self.subscribe(promise.resolve, promise.reject)
             
         queue.asyncAfter(deadline: .now() + interval) {
-            promise.reject(PromiseTimeoutError(timeoutInterval: interval))
+            promise.reject(error())
         }
         
         return promise
     }
     
     @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-    @inlinable public func timeout(_ duration: Duration, on queue: DispatchQueue = .global()) -> Promise<Output, Error> {
-        self.timeout(duration.timeInterval, on: queue)
+    @inlinable public func timeout<T: Error>(_ duration: Duration, error: @autoclosure @escaping () -> T, on queue: DispatchQueue = .global()) -> Promise<Output, Error> {
+        self.timeout(duration.timeInterval, error: error(), on: queue)
     }
 }
 #endif
